0972f1cafea143b2d7965fb411c5bda033019c2a
Matthew Gembický <matthew@Matthew-MacBook-Air.local>
2020-04-12 11:14:47 +0200

    make it better

--- app.js
+++ app.js
@@ -106,12 +106,12 @@ var Player = function(id,username){		//player data
 				self.slaped=GAMESPEED*3;
 				if(self.friend!==undefined){
 					self.friend=undefined;
-					var matcher = 0;
+					/*var matcher = 0;
 					for(var i in Player.list){
 						another = Player.list[i];
 						if(another.id===self.friend)
 							matcher = Player.list[i];
-					matcher.friend=undefined;
+					matcher.friend=undefined;*/
 				}
 
 			}
